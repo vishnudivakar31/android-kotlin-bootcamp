4fbd1cdfbf5ff2c07a4133db15ccedff83e3aa8d
vishnudivakar31 <vishnudivakar31@gmail.com>
2019-08-07 14:49:08 +0530

    tip calculator v1.0

--- bootcamp/TipCal/app/src/main/java/com/wanderingthinkter/tipcal/MainActivity.kt
+++ bootcamp/TipCal/app/src/main/java/com/wanderingthinkter/tipcal/MainActivity.kt
@@ -2,11 +2,23 @@ package com.wanderingthinkter.tipcal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.round
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        calculateTip.setOnClickListener {
+            var amount = amount.text.toString().toDouble()
+            val percentage = tipPercentage.text.toString().toDouble()
+            val tip = round(((percentage / 100) * amount) * 100) / 100
+            amount += tip
+            var resultTxt = getString(R.string.result).replace("{0}", tip.toString())
+            resultTxt = resultTxt.replace("{1}", amount.toString())
+            result.text = resultTxt
+        }
     }
 }
